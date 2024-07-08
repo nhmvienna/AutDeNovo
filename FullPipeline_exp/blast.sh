f6682700383360ae11741d012c65561ec4e2b0a8
MartinKapun <capoony@gmail.com>
2024-07-08 14:18:26 +0200

    Update blast.sh

--- FullPipeline_exp/blast.sh
+++ FullPipeline_exp/blast.sh
@@ -37,7 +37,7 @@ echo """
 
   ######## load dependencies #######
 
-  source ${Conda}/etc/profile.d/conda.sh
+  eval \"\$(conda shell.bash hook)\"
   conda activate envs/blast
 
   ######## run analyses #######
