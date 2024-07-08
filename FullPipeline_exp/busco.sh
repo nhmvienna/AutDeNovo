4c66730d4d6d13b5952efd8a5dd683b9bdedfbe4
MartinKapun <capoony@gmail.com>
2024-07-08 14:19:09 +0200

    Update busco.sh

--- FullPipeline_exp/busco.sh
+++ FullPipeline_exp/busco.sh
@@ -38,7 +38,7 @@ echo """
 
   ######## load dependencies #######
 
-  source ${Conda}/etc/profile.d/conda.sh
+  eval \"\$(conda shell.bash hook)\"
   conda activate envs/busco
 
   ######## run analyses #######
